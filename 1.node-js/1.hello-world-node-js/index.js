1eb06f12dad6fecc898cf46bd8ab2e009c3fcfbf
helper.dev <basakayush942@gmail.com>
2025-07-11 01:07:28 +0530

    code addded to index.js from hello from node js

--- 1.node-js/1.hello-world-node-js/index.js
+++ 1.node-js/1.hello-world-node-js/index.js
@@ -1,1 +1,13 @@
-console.log(`index`)
+console.log(`hello world node js`);
+
+
+const array = [1, 2, 3, 4]
+
+console.log(array,`array`)
+
+
+setTimeout(() => {
+  console.log(`code will run after 2 sec`)
+}, 2000);
+
+console.log(`this is the sync code`)
