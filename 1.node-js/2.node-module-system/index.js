ddfab8a5e923ee144ce22b05a12c6a03f8176a3a
helper.dev <basakayush942@gmail.com>
2025-07-11 09:00:41 +0530

    error handing added

--- 1.node-js/2.node-module-system/index.js
+++ 1.node-js/2.node-module-system/index.js
@@ -1,7 +1,14 @@
 import { div, mul, sub, sum } from "./first-module.js";
-
-
 console.log(sum(1, 9))
 console.log(sub(5, 2))
 console.log(mul(5, 2))
 console.log(div(0, 10))
+
+// error handling
+try {
+  console.log("trying to devide it by 0")
+  let result = div(0, 0);
+  console.log(result)
+} catch (error) {
+  console.log("Caught an error", error.message);
+}
