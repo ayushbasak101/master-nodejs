75dfaf4b1c3038172b8e399cda0e121e81623342
helper.dev <basakayush942@gmail.com>
2025-07-11 08:30:34 +0530

    export import added

--- 1.node-js/2.node-module-system/first-module.js
+++ 1.node-js/2.node-module-system/first-module.js
@@ -1,1 +1,18 @@
-console.log(`first module`)
+export function sum(a, b) {
+  return a + b;
+}
+
+function sub(a, b) {
+  return a - b;
+}
+
+
+function mul(a, b) {
+  return a * b;
+}
+
+function div(a, b) {
+  return a / b;
+}
+
+export { sub, mul, div };
--- 1.node-js/2.node-module-system/index.js
+++ 1.node-js/2.node-module-system/index.js
@@ -1,1 +1,7 @@
-console.log(`index module`)
+import { div, mul, sub, sum } from "./first-module.js";
+
+
+console.log(sum(1, 9))
+console.log(sub(5, 2))
+console.log(mul(5, 2))
+console.log(div(0, 10))
