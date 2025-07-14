72b49e805c4e3b8f1fc066440bfcd3fc8e6750b6
helper.dev <basakayush942@gmail.com>
2025-07-15 00:47:51 +0530

    mongodb schema and model added

--- 5.mondo-db-basics/index.js
+++ 5.mondo-db-basics/index.js
@@ -5,3 +5,14 @@ const MONGO_URI = "mongodb+srv://Ayush:Ayush%4023@ayushdb.nwlte0s.mongodb.net/?r
 mongoose.connect(MONGO_URI)
   .then(() => console.log("Database connection successful"))
   .catch((error) => console.error("Database connection error:", error));
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
+  isActive: Boolean,
+  tag: [String],
+  createAt: { type: Date, default: Date.now }
+})
+
+const userModel = mongoose.model("userModel", userSchema)
