c7823d22ffc8b7f68f8b16e063bdb1306623914f
helper.dev <basakayush942@gmail.com>
2025-07-17 11:24:21 +0530

    controller structure is made

---
+++ 6.Book-Store-API/controllers/book.controllers.js
@@ -0,0 +1,20 @@
+export const getAllBooks=async (req,res) => {
+  
+}
+
+export const getSingleBooksById=async (req,res) => {
+  
+}
+
+export const addBook=async (req,res) => {
+  
+}
+
+export const updateBook=async (req,res) => {
+  
+}
+
+export const deleteBook=async (req,res) => {
+  
+}
+
