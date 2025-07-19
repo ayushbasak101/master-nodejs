5a0c9740adc52dc0f7a31266dc82b1228213fa0d
helper.dev <basakayush942@gmail.com>
2025-07-19 10:00:01 +0530

    db conection added

--- 7.Authentication-&-Autharization/database/db.js
+++ 7.Authentication-&-Autharization/database/db.js
@@ -2,3 +2,15 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config()
 
+const MONGODB_URI = process.env.MONGODB_URI
+export const connectionDB = async () => {
+  try {
+    await mongoose.connect(MONGODB_URI)
+    console.log(`connection to DB is done`)
+
+  } catch (error) {
+    console.error(`error while connecting to DB`, error)
+    process.exit(1);
+  }
+}
+
--- 7.Authentication-&-Autharization/server.js
+++ 7.Authentication-&-Autharization/server.js
@@ -1,8 +1,10 @@
 import express from "express"
+import { connectionDB } from "./database/db.js"
 const app = express()
-
-
 const PORT = process.env.PORT || 9000
+
+// connection to MONOGODB
+connectionDB()
 
 app.listen(PORT, () => {
   console.log(`server running on PORT ${PORT}`)
