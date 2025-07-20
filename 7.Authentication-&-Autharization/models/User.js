b9870580e2f955495503d8b02440e6fcd5e9b0b5
helper.dev <basakayush942@gmail.com>
2025-07-20 18:35:23 +0530

    userSchema ready

---
+++ 7.Authentication-&-Autharization/models/User.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+export const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],// allow user or admin only
+    default: "user"
+  }
+}, { timestamps: true })
--- 7.Authentication-&-Autharization/server.js
+++ 7.Authentication-&-Autharization/server.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 9000
 // connection to MONOGODB
 connectionDB()
 
+
+
+
 app.listen(PORT, () => {
   console.log(`server running on PORT ${PORT}`)
 })
